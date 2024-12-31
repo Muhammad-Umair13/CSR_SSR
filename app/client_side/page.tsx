9edf759209eb7f92a946f2511735af79dc375f0b
Muhammad Umair <160301486+Muhammad-Umair13@users.noreply.github.com>
2024-12-31 15:56:24 +0500

    edit changes

--- app/client_side/page.tsx
+++ app/client_side/page.tsx
@@ -19,7 +19,7 @@ interface card {
 const Client_side = () => {
 
     const [data1, setdata] = useState<card[]>([])
-    const [loading ,setloading] = useState(true)
+    const [loading, setloading] = useState(true)
     useEffect(() => {
         const fetching = async () => {
             const data = await fetch("https://fakestoreapi.com/products");
@@ -30,29 +30,31 @@ const Client_side = () => {
         fetching();
     }, [])
 
-    if(loading){
+    if (loading) {
         return <div className="flex items-center justify-center min-h-screen bg-[#f5b94b]">
-             <div className="spinner-border animate-spin inline-block w-20 h-20 rounded-full text-black border-8 border-t-transparent border-black"></div> 
-             <span className="ml-4 text-[#ac802f] text-lg font-bold">Loading...</span> 
-             </div>
+            <div className="spinner-border animate-spin inline-block w-20 h-20 rounded-full text-black border-8 border-t-transparent border-black"></div>
+            <span className="ml-4 text-[#ac802f] text-lg font-bold">Loading...</span>
+        </div>
     }
     return (
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 p-4 bg-[#f5b94b]"> {
-            data1.map((value, index) => (
-                <div key={index} className="border-4 border-black rounded-lg shadow-lg p-4 bg-white">
-                    <img src={value.image} alt={value.title} className="h-40 w-1/2 m-auto rounded-t-lg" />
-                    <div className="p-4">
-                        <h2 className="text-lg font-bold mb-2">{value.title}</h2>
-                        <p className="text-gray-600 mb-2"><i>{value.category}</i></p>
-                        <p className="text-green-600 font-bold mb-2">${value.price}</p>
-                        <p className="text-gray-700 mb-2">{value.description}</p>
-                        <div className="flex items-center">
-                            <span className="text-yellow-500 font-bold mr-1">{value.rating.rate}</span>
-                            <span className="text-gray-600">({value.rating.count})</span>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 p-4 bg-[#f5b94b]">
+            {
+                data1.map((value, index) => (
+                    <div key={index} className="border-4 border-black rounded-lg shadow-lg p-4 bg-white">
+                        <img src={value.image} alt={value.title} className="h-40 w-1/2 m-auto rounded-t-lg" />
+                        <div className="p-4">
+                            <h2 className="text-lg font-bold mb-2">{value.title}</h2>
+                            <p className="text-gray-600 mb-2"><i>{value.category}</i></p>
+                            <p className="text-green-600 font-bold mb-2">${value.price}</p>
+                            <p className="text-gray-700 mb-2">{value.description}</p>
+                            <div className="flex items-center">
+                                <span className="text-yellow-500 font-bold mr-1">{value.rating.rate}</span>
+                                <span className="text-gray-600">({value.rating.count})</span>
+                            </div>
                         </div>
                     </div>
-                </div>
-            ))}
+                ))
+            }
         </div>
     );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ import Link from 'next/link';
 
 const HomePage = () => {
   return (
-    <div className="min-h-screen bg-gradient-to-r from-[#f5b94b] to-[#e29f23] flex flex-col items-center justify-center text-black p-6">
-      <h1 className="text-4xl font-bold mb-4">Welcome to Our Website</h1>
-      <p className="text-lg mb-8">Discover the power of client-side and server-side rendering with Next.js and Tailwind CSS.</p>
+    <div className="min-h-screen bg-gradient-to-r flex flex-col items-center justify-center bg-[#000000e8] p-6">
+      <h1 className="text-4xl font-bold mb-4 text-[#f5b94b] border-b-4">Welcome to Our Website</h1>
+      <p className="text-lg mb-8 text-[#f5b94b]">Discover the power of client-side and server-side rendering with Next.js and Tailwind CSS.</p>
 
       <div className="flex space-x-4">
         <Link href="/client_side" className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
@@ -18,12 +18,12 @@ const HomePage = () => {
         </Link>
       </div>
 
-      <div className="mt-10 text-center">
-        <h2 className="text-2xl font-semibold mb-4">Description</h2>
-        <p className="mb-2">Client-side rendering is a technique where the browser is responsible for rendering the HTML content using JavaScript.
+      <div className="mt-10 text-center w-[500px] max-sm:w-80">
+        <h2 className="text-2xl font-semibold mb-4 text-[#f5b94b]">Description</h2>
+        <p className="mb-5 text-[#f5b94b]"><u>Client-side rendering</u>  is a technique where the browser is responsible for rendering the HTML content using JavaScript.
           <br />
           This process occurs after the page has been fully loaded in the clients browser.</p>
-        <p className="mb-2">Server-side rendering is a technique where the server generates the complete HTML content for a page and sends it to the clients browser.
+        <p className="mb-5 text-[#f5b94b]"><u>Server-side rendering</u> is a technique where the server generates the complete HTML content for a page and sends it to the clients browser.
           <br />This process occurs before the page is fully loaded</p>
         {/* <p>Join us on a journey to explore the potentials of client-side and server-side rendering!</p> */}
       </div>
